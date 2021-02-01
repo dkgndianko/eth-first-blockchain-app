8978c2707dcbc63af963a0aa48c6e978a373dc73
Mouhamad THIAM <mohamed@ctsfares.com>
2021-02-01 15:16:20 +0000

    implement tests

---
+++ test/todoList.test.js
@@ -0,0 +1,23 @@
+const TodoList = artifacts.require("./TodoList.sol");
+contract('TodoList', (accounts) => {
+    before(async () => {
+        this.todoList = await TodoList.deployed();
+    })
+
+    it('deploys successfully', async () => {
+        const address = await this.todoList.address;
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, '');
+        assert.notEqual(address, null);
+        assert.notEqual(address, undefined);
+    
+    })
+
+    it('lists tasks', async () => {
+        const taskCount = await this.todoList.taskCount();
+        const lastTask = await this.todoList.tasks(taskCount);
+        assert.equal(taskCount.toNumber(), lastTask.id.toNumber());
+        assert.equal(lastTask.content, "Mouhamad's first task");
+        assert.equal(lastTask.completed, false);
+    })
+});
